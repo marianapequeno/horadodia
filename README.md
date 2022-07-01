361dc3c8c5c0efe100b4dbbc361d61089186a777
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-07-01 11:23:23 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio hora do dia
+Exercício do módulo D - Curso JavaScript do Curso em Vídeo
---
+++ script.js
@@ -0,0 +1,32 @@
+const data = new Date();
+const hora = data.getHours();
+const minuto = data.getMinutes(2);
+
+const horas = (hora<10) ? `0${hora}` : hora;
+const minutos = (minuto<10) ? `0${minuto}` : minuto;
+
+let horario = document.getElementById("horario");
+horario.innerHTML = `Agora são ${horas}h${minutos}.`;
+
+console.log(`Hora Atual: ${horas}:${minutos}`);
+
+const imagem = document.querySelector("img");
+const varCss = document.documentElement.style;
+if(hora>=6 && hora<12){
+  imagem.src="imagens/manha.jpg";
+  varCss.setProperty("--cor-fundo", "#7C379E");
+  varCss.setProperty("--cor-textos", "#C06AEB");
+  varCss.setProperty("--cor-hover", "#A955D4");
+}else if(hora>=12 && hora<18){
+  imagem.src="imagens/tarde.jpg";
+  imagem.setAttribute("alt", "Foto de uma tarde em dia ensolarado")
+  varCss.setProperty("--cor-fundo", "#EEEB53");
+  varCss.setProperty("--cor-textos", "#9E9C08");
+  varCss.setProperty("--cor-hover", "#B8B509");
+}else{
+  imagem.src="imagens/noite.jpg";
+  imagem.setAttribute("alt", "Foto da cidade à noite")
+  varCss.setProperty("--cor-fundo", "#4F429E");
+  varCss.setProperty("--cor-textos", "#7563EB");
+  varCss.setProperty("--cor-hover", "#5B4DB8");
+}
